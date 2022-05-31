b0313707210311b73818b2f7f088826968a6d9d5
Borja Gómez Muñoz <borja@reverscore.com>
2022-05-31 12:05:25 +0200

    Adding bootstrap. Creating a clickable polygon

--- app/src/components/GoogleMap.vue
+++ app/src/components/GoogleMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container-fluid mt-5">
     <div>
       <h2>Atforestry</h2>
     </div>
@@ -8,6 +8,7 @@
       ref="mapRef"
       :center='center'
       :zoom='12'
+      :draggable="false"
       :options="{
         zoomControl: false,
         mapTypeControl: false,
@@ -17,16 +18,27 @@
         fullscreenControl: false,
         disableDefaultUi: true
       }"
-      @click="clickOnMap"
       style='width:100%;  height: 400px;'
-      map-type-id="terrain"
-    />    
+      map-type-id="satellite"
+      max-zoom="12"
+      min-zoom="12"
+    >
+      <GMapPolygon
+          :paths="paths"
+          @click="clickOnMap"
+          :options="optionsPolygon"
+      />
+    </GMapMap>    
   </div>
 </template>
 
 <script>
 export default {
   name: 'GoogleMap',
+  mounted() {
+    let map = this.$refs.mapRef
+    console.log(map)
+  },
   methods: {
     clickOnMap(e) {
       let lat = e.latLng.lat()
@@ -36,7 +48,21 @@ export default {
   },
   data() {
     return {
-      center: { lat: -53.08, lng: -3.16 },
+      center: { lat: 41.38, lng: 2.16 },
+      paths: [
+        { lat: 41.3819, lng: 2.0752 },
+        { lat: 41.4398, lng: 2.1641 },
+        { lat: 41.4169, lng: 2.2352 },
+        { lat: 41.3195, lng: 2.1471 },
+        { lat: 41.3819, lng: 2.0752 }
+      ],
+      optionsPolygon: {
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+      }
     }
   },  
 }
--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 const app = createApp(App)
 app.use(VueGoogleMaps, {
   load: {
